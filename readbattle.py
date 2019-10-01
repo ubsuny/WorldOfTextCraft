df4443439974d326a95c53db73d0564b0d85de22
Salvatore Rappoccio <rappoccio@gmail.com>
2019-09-30 23:48:17 -0400

    Read battle script now has sensible ndarrays

--- readbattle.py
+++ readbattle.py
@@ -1,22 +1,30 @@
 import json
+import numpy as np
+
+
 with open('battlelog.txt') as json_file:
     data = json.load(json_file)
 
-charnames = ["Arthas", "Fordring", "Thrall", "Mograine"]
-
 
 turns = data["Turns"]
+charnames = list(turns[0].keys())
+charnames.remove("Turn")
 turnheaders = [ s for s in turns[0][charnames[0]] ]
 print(turnheaders)
 
+nturns = len( data["Turns"] )
 
+npdata = np.zeros( (len(charnames),nturns,len(turnheaders)) )
 
-for turn in turns:
+for iturn,turn in enumerate( turns ):
     turnval = turn["Turn"]
     print ("------------ turn : ", turnval, " ---------------")
-    for charname in charnames:
+    for icharname, charname in enumerate(charnames):
         s = "==== %10s :" % (charname)
         vals = turn[charname]
         for iact,val in vals.items():
-            s += "%3d," % (sum(val) )
+            s += "%3d," % (sum(val) )        
+            npdata[icharname,turnval,turnheaders.index(iact)] = sum(val)
         print(s)
+
+print(npdata)
